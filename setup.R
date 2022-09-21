0673d18fdfd20af7f67be6e2e00af1df04631e91
Esturban <info@evadvisory.ca>
2022-09-20 19:31:41 -0500

    Dependencies
    Adding the script runtime dependencies into the setup file

---
+++ setup.R
@@ -0,0 +1,15 @@
+# setup.R
+#Library dependencies
+require(purrr)
+require(dplyr)
+require(jsonlite)
+require(here)
+require(DT)
+
+
+#' 
+#' Extracting the domain name from a string 
+#' @description The domain name is an element of a URL necessary for categorizing clients. This function will strip all of the unnecessary components of the URL so that the domain name remains
+domain <-
+  function(x)
+    strsplit(gsub("http://|https://|www\\.", "", x), "/")[[c(1, 1)]]
