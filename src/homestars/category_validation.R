ce1e9bd3c407e9172f06b56747b3381e9633504b
Esturban <info@evadvisory.ca>
2022-11-14 13:29:49 -0500

    Homestars name handling
    Error writing the table name due to weird punctuation

--- src/homestars/category_validation.R
+++ src/homestars/category_validation.R
@@ -31,7 +31,7 @@ lapply(cities, function(x) {
   city_dataset%>%dplyr::mutate(page_status = purrr::map_chr(.x = page_status,~rjson::toJSON(.x)),
                                update_time = Sys.time())->city_dataset
   
-  readr::write_csv(city_dataset,file = here::here("data","homestars","categories",paste0("cat_",x,".csv")))
+  readr::write_csv(city_dataset,file = here::here("data","homestars","categories",paste0("cat_",gsub("[[:punct:]]|[[:blank:]]","",x),".csv")))
   
   
 }) %>% dplyr::bind_rows(.) -> cities_categories_validated
