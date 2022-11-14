422dc0c3b96541eb9245e72d76d1df81ba630141
Esturban <info@evadvisory.ca>
2022-11-14 11:48:50 -0500

    Dependency +Trigger
    Updating the dependency for RJSON and the last max trigger

--- src/homestars/homestar_categories.R
+++ src/homestars/homestar_categories.R
@@ -33,8 +33,6 @@ city_cats <- lapply(1:length(cities), function(x) {
   )
 }) %>% dplyr::bind_rows(.)
 
-last_max <- 0
-
 #Testing not enabled at this time
 if(test_it)source(here::here("src","homestars","TEST","test_homestar_categories.R"),F)
 
