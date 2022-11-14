c588dea8825b838236504833f8c47012502c0216
Esturban <info@evadvisory.ca>
2022-11-14 10:44:59 -0500

    Homestars
    First pass files

---
+++ src/homestars/TEST/test_homestar_categories.R
@@ -0,0 +1,34 @@
+for (i in 1:100)
+{
+  if (test_it)
+  {
+    # browser()
+    #Random test link to see what the page outcomes look like in UI vs scrape.
+    city_cats[sample.int(nrow(all_city_cats), 1), 'Link'] -> test_link
+    #print the testable link
+    print(test_link)
+    #Scrape the testable link
+    test_link %>% read_html -> page_test
+    #Unlink the test page
+    unlink(test_link)
+    
+    #Gather the results of the scraped page
+    page_test %>% html_nodes(
+      "#search-page > div.search-page__head-container > div.search-page__result-links > div"
+    ) %>% html_attr("data-react-props") %>% rjson::fromJSON(.) %>% .[['totalHits']] %>%
+      as.numeric(.) ->
+      test_number
+    #Find the max in 100 iterations of 1890 categories
+    if (last_max < test_number){
+      
+      last_max <- test_number
+    }    
+    page_test %>% html_nodes("#results-sponsors > section > section > div") %>%
+      html_attr("data-react-props") %>% lapply(., rjson::fromJSON)%>%unique(.) -> test_page_1
+    class(test_page_1)
+    if (last_max == test_number)largest_page<-test_page_1
+    print(sapply(test_page_1,function(x)x[['company']][['direct_phone']]))
+  }
+}
+
+
---
+++ src/homestars/homestar_categories.R
@@ -0,0 +1,39 @@
+# hs_categories
+#Scraping all of the homestars categories
+#
+sapply(list.files(here::here("src","fns"),full.names = T),source,local = F)
+test_it <- T
+#Gather all of the categories up front
+"https://homestars.com/on/toronto/categories" %>% read_html -> all_cat_page
+#Gather all of the toronto specifuc URLs
+all_cat_page %>% html_nodes("body > div.single-column-page > div > div > ul > li > a") %>%
+  html_attr("href") %>% paste0("https://homestars.com", .) -> toronto_urls
+
+cat_suffix<-gsub("https://homestars.com/on/toronto","",toronto_urls)
+#Get all of the named categories
+all_cat_page %>% html_nodes(".category-group__category-name") %>% html_text ->
+  all_cats
+#Replace toronto with a sprintable text
+cat_sprint_urls <- gsub("toronto", "%s", toronto_urls)
+
+
+'https://homestars.com/on/cities'%>%read_html%>%html_nodes('.city__link')%>%html_attr("href")%>%paste0("https://homestars.com",.)%>%gsub("[[:blank:]]","%20",.)->city_links
+
+#Corresponding cities in Ontario with whom to start
+cities <-'https://homestars.com/on/cities'%>%read_html%>%html_nodes('.city__link')%>%html_attr("href")%>%gsub("/on/","",.)
+# x<-1
+#Create all of the city categories in a data frame
+city_cats <- lapply(1:length(cities), function(x) {
+   paste0(city_links[x],cat_suffix)-> cat_links
+  data.frame(
+    City = rep(cities[x], length(all_cats)),
+    Category = all_cats,
+    Link = cat_links,
+    stringsAsFactors = F
+  )
+}) %>% dplyr::bind_rows(.)
+
+last_max <- 0
+
+if(test_it)source(here::here("src","homestars","TEST","test_homestar_categories.R"),F)
+
