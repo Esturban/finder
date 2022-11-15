78886dafea841d82d02c3c705a1df9c1c4c34f25
Esturban <info@evadvisory.ca>
2022-11-15 13:09:46 -0500

    Homestars Job
    Now locally blocked, gg

--- src/homestars/category_validation.R
+++ src/homestars/category_validation.R
@@ -1,45 +1,70 @@
 # homestar_category_validation
-start<-Sys.time()
-lapply(cities, function(x) {
-  #City being analyzed 
-  print(x)
+start <- Sys.time()
+lapply(cities[sample.int(length(cities),350)], function(x) {
+  #City being analyzed
+  if (!file.exists(here::here(
+    "data",
+    "homestars",
+    "categories",
+    paste0("cat_", gsub("[[:punct:]]|[[:blank:]]", "", x), ".csv")
+  ))) {
   
-  intermediate_city_categories <- city_cats %>% dplyr::filter(City == x)
-  
-  # intermediate_city_categories[1:10,]%>%
-  intermediate_city_categories %>%
-    dplyr::mutate(
-      #Download the full web page
-      # page = purrr::map(.x = Link,~page_dl(.x,skip = T)),
-      #RCurl Test - Determine if the page exists or not
-      # page_test = purrr::map(.x = Link,~{
-      #   RCurl::url.exists(.x)->res
-      #   print(res)
-      #   return(res)
-      # }),
-      #HTTR Test - Determine if the page exists or not
-      page_status = purrr::map(.x = Link,  ~ {
-        httr::http_status(httr::GET(.x)) -> res
-        # print(res)
-        return(res)
-      }),
-      status_code = purrr::map(.x = page_status,  ~ gsub(
-        "[\\(\\)]", "", regmatches(.x$message, gregexpr("\\(.*?\\)", .x$message))[[1]]
-      ))
-    )->city_dataset
-  
-  city_dataset%>%dplyr::mutate(page_status = purrr::map_chr(.x = page_status,~rjson::toJSON(.x)),
-                               update_time = Sys.time())->city_dataset
-  if(!dir.exists(here::here("data","homestars","categories"))) dir.create(here::here("data","homestars","categories"))
-  readr::write_csv(city_dataset,file = here::here("data","homestars","categories",paste0("cat_",gsub("[[:punct:]]|[[:blank:]]","",x),".csv")))
+    print(x)
+    intermediate_city_categories <-
+      city_cats %>% dplyr::filter(City == x)
+    
+    # intermediate_city_categories[1:10,]%>%
+    intermediate_city_categories %>%
+      dplyr::mutate(
+        #Download the full web page
+        # page = purrr::map(.x = Link,~page_dl(.x,skip = T)),
+        #RCurl Test - Determine if the page exists or not
+        # page_test = purrr::map(.x = Link,~{
+        #   RCurl::url.exists(.x)->res
+        #   print(res)
+        #   return(res)
+        # }),
+        #HTTR Test - Determine if the page exists or not
+        page_status = purrr::map(.x = Link,  ~ {
+          httr::http_status(httr::GET(.x)) -> res
+          # print(res)
+          return(res)
+        }),
+        status_code = purrr::map(.x = page_status,  ~ gsub(
+          "[\\(\\)]", "", regmatches(.x$message, gregexpr("\\(.*?\\)", .x$message))[[1]]
+        ))
+      ) -> city_dataset
+    
+    city_dataset %>% dplyr::mutate(
+      page_status = purrr::map_chr(.x = page_status,  ~ rjson::toJSON(.x)),
+      update_time = Sys.time()
+    ) -> city_dataset
+    if (!dir.exists(here::here("data", "homestars", "categories")))
+      dir.create(here::here("data", "homestars", "categories"))
+    readr::write_csv(city_dataset,
+                     file = here::here(
+                       "data",
+                       "homestars",
+                       "categories",
+                       paste0("cat_", gsub("[[:punct:]]|[[:blank:]]", "", x), ".csv")
+                     ))
+  }
   
   
 }) %>% dplyr::bind_rows(.) -> cities_categories_validated
 
-end<-Sys.time()
-end-start
+end <- Sys.time()
+end - start
 
-readr::write_csv(cities_categories_validated,file = here::here("data","homestars","categories","all_cities_categories.csv"))
+readr::write_csv(
+  cities_categories_validated,
+  file = here::here(
+    "data",
+    "homestars",
+    "categories",
+    "all_cities_categories.csv"
+  )
+)
 # head(city_cats)
 # httr::httr::http_status(
 #   httr::GET(
