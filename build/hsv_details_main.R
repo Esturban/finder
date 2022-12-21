2d599d37201f7a97cfa8c8d013dc444dd5e516e4
Esturban <info@evadvisory.ca>
2022-12-21 07:34:20 -0500

    Commenting
    Complete the commenting for the HS validation job

--- build/hsv_details_main.R
+++ build/hsv_details_main.R
@@ -1,7 +1,10 @@
 # hsv_details
 
 #HSV = Homestars Validation of Company details 
-#
+# This job is used to scrape the category pages from homestars 
+# and download the company information that is available publicly 
+# through a genetic algorithm that estimates how many additional pages
+# exist for a corresponding category
 source(here::here("setup.R"), F)
 
 hs_load <-F
