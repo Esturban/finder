f4b4b31cb7a3bdf69f389deac35ec4f1e5224249
Esturban <info@evadvisory.ca>
2023-02-19 11:17:18 -0500

    Documentation
    Updating the documentation of the scripts so that it can be included during runtime

--- src/fns/node_attrs.R
+++ src/fns/node_attrs.R
@@ -1,11 +1,33 @@
-node_attrs<-function(x,selected = "head > link, head > script",condition = T){
-  if (condition){
-    tryCatch({x %>% html_nodes(selected) %>% html_attrs(.) %>% return(.)},error=function(e){
+#' Extract node attributes from HTML file
+#'
+#' This function accepts an HTML file and a CSS selector, and returns a list of attributes for each node that matches the selector.
+#'
+#' @param x An HTML file.
+#' @param selected A CSS selector to use to select nodes to extract attributes from.
+#' @param condition A logical value indicating whether to apply error handling to the function.
+#'
+#' @return A list of attributes for each node that matches the selected CSS selector.
+#'
+#' @examples
+#' node_attrs(x = read_html("<html><head><link rel='stylesheet' href='style.css'></head><body><p class='paragraph'>Hello, world!</p></body></html>"),
+#'            selected = "head > link, head > script",
+#'            condition = TRUE)
+#'
+#' @export
+node_attrs <- function(x, selected = "head > link, head > script", condition = TRUE) {
+  if (condition) {
+    tryCatch({
+      x %>% 
+        html_nodes(selected) %>% 
+        html_attrs(.) %>% 
+        return(.)
+    }, error = function(e) {
       print(condition)
       print(x)
       print(e)
       return(NULL)
     })
-}  else
+  } else {
     return(NULL)
+  }
 }
--- src/fns/social_user.R
+++ src/fns/social_user.R
@@ -1,16 +1,32 @@
-#From the page attributes, determine if the corresponding social source exists
-#
-#If the item exists, then find the source and remove parts of the stop content
-#in an unclear sense
-
+#' Determine if a social media source exists
+#'
+#' This function takes in page attributes and determines if the corresponding social media source exists. If the source exists, it is cleaned up by removing parts of the stop content in an unclear sense. The resulting output is the cleaned-up social media link.
+#'
+#' @param x A list of page attributes.
+#' @param social A character string specifying the social media platform to search for.
+#' @param verbose A logical value indicating whether to print verbose output during the function's execution.
+#' @param stopwords A character vector specifying stop content to remove from the social media link.
+#' @param social_min_char An integer specifying the minimum number of characters required in the social media link.
+#'
+#' @return A cleaned-up social media link if one exists, otherwise `NA_character_`.
+#'
+#' @examples
+#' social_user(x = list(href = "https://www.instagram.com/example/",
+#'                      src = "https://www.example.com/assets/images/instagram.png"),
+#'             social = "instagram",
+#'             verbose = FALSE,
+#'             stopwords = character(0),
+#'             social_min_char = 2)
+#'
+#' @export
 social_user <-
   function(x,
            social = "instagram",
-           verbose = F,
+           verbose = FALSE,
            stopwords = character(0),
            social_min_char = 2) {
     lapply(x, function(iter) {
-      #If the `href` attribute exists in the list of attributes
+      # If the `href` attribute exists in the list of attributes
       if ("href" %in% names(iter)) {
         if (grepl(social, iter[["href"]])) {
           return(iter[["href"]])
@@ -18,7 +34,7 @@ social_user <-
           return(NULL)
         }
       }
-      #If the `src` attribute exists in the list of attributes
+      # If the `src` attribute exists in the list of attributes
       if ("src" %in% names(iter)) {
         if (grepl(social, iter[["src"]])) {
           return(iter[["src"]])
@@ -27,18 +43,18 @@ social_user <-
         }
       }
     }) %>%
-      #Remove all of the empty elements
+      # Remove all of the empty elements
       purrr::compact(.) %>%
       unlist(.) %>%
       gsub("/p/\\w+|scontent\\w+|/reel/\\w+|more.ctv\\w+", "", .) %>%
-      #If there are no stopwords, then pass the link through as normal
+      # If there are no stopwords, then pass the link through as normal
       {
         ifelse(length(stopwords) >= 1 , gsub(paste0(stopwords, collapse = "|"), "", .), .)
       } %>%
-      #The text must have a minimum of 2 characters
+      # The text must have a minimum of 2 characters
       .[nchar(.) > social_min_char] %>%
       unique(.) -> res
-    if (verbose == T)
+    if (verbose == TRUE)
       print(res)
     
     if (identical(res, character(0)))
